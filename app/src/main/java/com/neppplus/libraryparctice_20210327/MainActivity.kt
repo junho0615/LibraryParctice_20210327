a21c2e7bffe513eb3e966480bbbc16d9fd947eaf
홍준호 <0615wnsgh@gmail.com>
2021-03-27 15:57:57 +0900

    전화걸기 추가

--- app/src/main/java/com/neppplus/libraryparctice_20210327/MainActivity.kt
+++ app/src/main/java/com/neppplus/libraryparctice_20210327/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.neppplus.libraryparctice_20210327
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        callBtn.setOnClickListener {
+
+            val phonenum = phonenumTxt.text.toString()
+            val myUri = Uri.parse("tel:${phonenum}")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
+
+        profilePictureImg.setOnClickListener {
+//            사진을 크게보는 화면으로 이동.
+            val myIntent = Intent(this, ViewprofilePicture::class.java)
+            startActivity(myIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/neppplus/libraryparctice_20210327/ViewprofilePicture.kt
@@ -0,0 +1,11 @@
+package com.neppplus.libraryparctice_20210327
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewprofilePicture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewprofile_picture)
+    }
+}
